c0a2a7faf7300563ca400b506e5f409cab88d56f
forest079 <bluegx08@gmail.com>
2023-08-31 17:08:29 +0800

    add: navbar、banner

---
+++ main.js
@@ -0,0 +1,9 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+
+// plugin -
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+window.AOS = AOS;
